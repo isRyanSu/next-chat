a640b9b55860816195fb8ed4b1256c1568880b6b
Ryan Su <isRyanSu@gmail.com>
2024-04-25 11:25:06 +0800

    chore: add speed insights and analytics

--- components/global/Provider.tsx
+++ components/global/Provider.tsx
@@ -1,14 +1,21 @@
 import { ThemeProvider as NextThemesProvider } from 'next-themes'
 import { type ThemeProviderProps } from 'next-themes/dist/types'
 
+import { SpeedInsights } from '@vercel/speed-insights/next'
+import { Analytics } from '@vercel/analytics/react'
+
 function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>
 }
 
 export default function Provider({ children }: { children: React.ReactNode }) {
   return (
-    <ThemeProvider attribute="class" defaultTheme="system">
-      {children}
-    </ThemeProvider>
+    <>
+      <ThemeProvider attribute="class" defaultTheme="system">
+        {children}
+      </ThemeProvider>
+      <SpeedInsights />
+      <Analytics />
+    </>
   )
 }
